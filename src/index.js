e322718d972796562c5c1618685351be34ae8795
NG <nathan.gervais@connect.auto>
2020-03-05 01:12:30 +0000

    axios all method now allows for multiple objects

--- src/index.js
+++ src/index.js
@@ -22,35 +22,49 @@ const mapData = data => {
   };
 };
 
-const getListings = url => {
-  const listingId = url.match(/\d{8}/g);
+const getListings = async urls => {
+  let currentListingId;
+  const calls = urls.map(url => {
+    currentListingId = url.match(/\d{8}/g);
+    return axios
+      .get(
+        `https://www.airbnb.co.uk/api/v2/pdp_listing_details/${currentListingId}?_format=for_rooms_show&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&`
+      )
+      .catch(error => {
+        if (error.response.status === 404) {
+          console.log(
+            `Listing with listing id: ${currentListingId} not found.`
+          );
+        }
+        if (error.response.status === 503) {
+          console.log(
+            `Unable to process listing with id:  ${currentListingId}, Service is currently unavailable`
+          );
+        }
+      });
+  });
   return axios
-    .get(
-      `https://www.airbnb.co.uk/api/v2/pdp_listing_details/${listingId}?_format=for_rooms_show&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&`
+    .all(calls)
+    .then(arrayOfResponses =>
+      arrayOfResponses.map(response => {
+        const {
+          data: { pdp_listing_detail }
+        } = response;
+        return mapData(pdp_listing_detail);
+      })
     )
-    .then(response => {
-      const {
-        data: { pdp_listing_detail: listing }
-      } = response;
-
-      return mapData(listing);
-    })
     .catch(error => {
-      if (error.response.status === 404) {
-        console.log(`Listing with listing id: ${listingId} not found.`);
-      }
-      if (error.response.status === 503) {
-        console.log(
-          `Unable to process listing with id: ${listingId}, Service is currently unavailable`
-        );
-      }
+      return error;
     });
 };
 
-async function getListingData(url) {
-  const listingData = await getListings(url);
+async function getListingData(urls) {
+  const listingData = await getListings(urls);
 
   console.log(listingData);
 }
 
-getListingData('https://www.airbnb.co.uk/rooms/19278160?s=51');
+getListingData([
+  'https://www.airbnb.co.uk/rooms/19278160?s=51'
+  // 'https://www.airbnb.co.uk/rooms/00000000?s=51'
+]);
